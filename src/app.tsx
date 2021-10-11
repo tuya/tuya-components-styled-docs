8247164992c69a77abe9803e12a7cc2461ef6fd6
muhai <muhai.li@tuya.com>
2021-10-11 15:12:12 +0800

    chore: update

--- src/app.tsx
+++ src/app.tsx
@@ -15,11 +15,13 @@ declare const HEARTBEAT: {
 };
 declare const __Device__: { refresh(ref: number): void };
 
-HEARTBEAT.delta = 1000;
-// 心跳检测，如果demo断开连接，则自动尝试刷新重连
-HEARTBEAT.connect(alive => {
-  if (!alive) {
-    console.log('已断开连接，正在重连');
-    __Device__.refresh(HEARTBEAT.ref);
-  }
-});
+if (typeof window !== 'undefined') {
+  HEARTBEAT.delta = 1000;
+  // 心跳检测，如果demo断开连接，则自动尝试刷新重连
+  HEARTBEAT.connect(alive => {
+    if (!alive) {
+      console.log('已断开连接，正在重连');
+      __Device__.refresh(HEARTBEAT.ref);
+    }
+  });
+}
